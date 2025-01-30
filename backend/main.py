b29d62f04c79530b36c1ae99293b8a28405b3d9a
varun252k4 <varun.252k4@gmail.com>
2025-01-30 16:24:46 +0530

    schema changed

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 # from prisma import Prisma
 # from prisma.models import Student, PrintJob, CanteenOrder, Payment
+# from Config.Connection import prisma_connection
 from pydantic import BaseModel
 from typing import List
 import os
